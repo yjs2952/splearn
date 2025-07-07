8e13995f5da85e0884fc8656bb240ddc90d9bf98
bro.fallz <bro.fallz@kakaocorp.com>
2025-07-07 13:12:30 +0900

    Initial commit

---
+++ src/main/kotlin/com/study/splearn/splearn/SplearnApplication.kt
@@ -0,0 +1,11 @@
+package com.study.splearn.splearn
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SplearnApplication
+
+fun main(args: Array<String>) {
+    runApplication<SplearnApplication>(*args)
+}
---
+++ src/test/kotlin/com/study/splearn/splearn/SplearnApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.study.splearn.splearn
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SplearnApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
