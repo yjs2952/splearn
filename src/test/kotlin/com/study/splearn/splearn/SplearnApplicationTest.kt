f45d6cc4d36af8867c732d9084eb8449888931a7
yjs2952 <yjs2952@gmail.com>
2025-07-31 13:33:11 +0900

    rollback

---
+++ src/test/kotlin/com/study/splearn/splearn/SplearnApplicationTest.kt
@@ -0,0 +1,27 @@
+package com.study.splearn.splearn
+
+import org.mockito.Mockito
+import org.springframework.boot.SpringApplication
+import org.springframework.context.ConfigurableApplicationContext
+import kotlin.test.Test
+
+class SplearnApplicationTest {
+
+    @Test
+    fun `main에서 SpringApplication이 실행되는지 확인`() {
+        // 정적 메서드 mocking
+        Mockito.mockStatic(SpringApplication::class.java).use { mock ->
+            mock.`when`<Any> {
+                SpringApplication.run(SplearnApplication::class.java, *arrayOf<String>())
+            }.thenReturn(Mockito.mock(ConfigurableApplicationContext::class.java))
+
+            // 메인 실행
+            main(arrayOf())
+
+            // 호출 여부 검증
+            mock.verify {
+                SpringApplication.run(SplearnApplication::class.java, *arrayOf<String>())
+            }
+        }
+    }
+}
